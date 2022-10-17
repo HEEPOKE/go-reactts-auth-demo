bbbf6a5abaffea02d266830cf0dbb333df4db01a
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-10-18 02:14:38 +0700

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import HomeScreen from './screens/HomeScreen'
-import LoginScreen from './screens/LoginScreen'
-import SignupScreen from './screens/SignScreen'
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignScreen";
 
 const App = () => {
   return (
@@ -14,14 +13,14 @@ const App = () => {
       <Header />
       <main>
         <Container>
-          <Route path='/' exact component={HomeScreen} />
-          <Route path='/signup' component={SignupScreen} />
-          <Route path='/login' component={LoginScreen} />
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/signup" component={SignupScreen} />
+          <Route path="/login" component={LoginScreen} />
         </Container>
       </main>
       <Footer />
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
